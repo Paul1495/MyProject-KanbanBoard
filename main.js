011c7419817818f50a1a329a83681c9a50082d97
PaulKien <mtrungkien1014@gmail.com>
2023-03-06 16:59:45 +0700

    Update Main JS

--- main.js
+++ main.js
@@ -13,6 +13,21 @@
 // }
 
 function recordDataInput() {
+    let inputDescription = document.getElementById('input-description').value;
+    let inputTypeProduct = document.getElementById('inputTypeProduct').value;
+    
+if (inputDescription === '') {
+    document.getElementById('inputDescription-error').innerHTML = 'Pls enter the description';
+} else {
+    document.getElementById('inputDescription-error').innerHTML = '';
+}
+
+if (inputTypeProduct === '') {
+    document.getElementById('inputType-error').innerHTML = 'Pls enter the type of Kanban board';
+} else {
+    document.getElementById('inputType-error').innerHTML = '';
+}
+
 
 }
 
